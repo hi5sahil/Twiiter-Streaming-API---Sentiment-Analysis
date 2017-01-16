ae7510ffc05be81b1450bb711518fbbf783c0c08
hi5sahil <hi5sahil@gmail.com>
2017-01-16 15:16:54 -0600

    Twiiter Live API

---
+++ Tweet Fetch for PySpark Streaming.py
@@ -0,0 +1,54 @@
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import socket
+import json
+
+
+consumer_key = "xQNiAHTS0JDZ9P1SRIy3ZJ5c0"
+consumer_secret = "w0TSDigLm71AqEQbiQ5dQhXMsbg0a0CpCfoHLNyXVH2OtM49NQ"
+access_token = "79980108-5H5NwHvcUNI8QN6DW61EaCkGGjwC0gixks46eR6jC"
+access_secret = "TBDRr9hZYcS3EvP4M7qz6tK0ouhvbol41VOckFTSAH8ox"
+
+
+class TweetsListener(StreamListener):
+
+  def __init__(self, csocket):
+      self.client_socket = csocket
+
+  def on_data(self, data):
+      try:
+          msg = json.loads( data )
+          print( msg['text'].encode('utf-8') )
+          self.client_socket.send( msg['text'].encode('utf-8') )
+          return True
+      except BaseException as e:
+          print("Error on_data: %s" % str(e))
+      return True
+
+  def on_error(self, status):
+      print(status)
+      return True
+
+def sendData(c_socket):
+  auth = OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_secret)
+
+  twitter_stream = Stream(auth, TweetsListener(c_socket))
+  twitter_stream.filter(track=['messi','cr7','ronaldo','zlatan','griezmann','suarez','neymar','bale'])
+
+if __name__ == "__main__":
+  s = socket.socket()          #Create a socket object
+  host = "192.168.1.141"       #Get local machine name
+  port = 5555                  #Reserve a port for your service.
+  s.bind((host, port))         #Bind to the port
+
+  print("Listening on port: %s" % str(port))
+
+  s.listen(5)                  #Now wait for client connection.
+  c, addr = s.accept()         #Establish connection with client.
+
+  print( "Received request from: " + str( addr ) )
+
+  sendData( c )
